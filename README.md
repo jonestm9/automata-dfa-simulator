7bd4a0d14d123ec30911a848933acd5fb5b27e31
trevo <trevor.m.jones@vanderbilt.edu>
2023-11-20 23:34:50 -0500

    updating testing and readme

--- README.md
+++ README.md
@@ -5,10 +5,17 @@ To run, make sure you have Python installed.
 Then, run
 
 ```
-python3 dfa-sim.py
+python3 dfa_sim.py
 ```
 
 in your terminal when in the repo's directory.
 
+Once running, you'll be prompted to input your DFA's alphabet, states, start state, accepting states, and transitions.
+Then, you'll be prompted to enter strings that your DFA will either accept or reject as being recognized in the language.
+
+To run the testing suite, run 
+```
+python3 testing.py
+```
+
 This was written for CS3252 - Theory of Automata, Formal Languages, and Computation.
-Feel free to reach out with feedback or questions at trevorjones1211@gmail.com.
--- testing.py
+++ testing.py
@@ -2,14 +2,25 @@ from dfa_sim import recognize_string
 
 def test_recognition():
     test_inputs = [
+        # Each test array includes the following fields:
+        # 1. The string to test in the DFA (aka the user's inputted string)
+        # 2. The alphabet of the DFA, in set format
+        # 3. The start state of the DFA, in string format
+        # 4. The accepting states of the DFA, in set format
+        # 5. The transitions of the DFA, in dictionary format (key: tuple, value: string)
+        # 6. The expected result of the input string in our recognizer function based on the provided DFA specifications
+        
+        
+        # Test cases 1-3: Testing different strings on a simple DFA that only accepts strings with 0 or 1 of length 1
         ["0", {"0","1"}, 'q0', {'q1'}, {('q0', '1'): 'q1', ('q0', '0'): 'q1'}, True],
         ["1", {"0","1"}, 'q0', {'q1'}, {('q0', '1'): 'q1', ('q0', '0'): 'q1'}, True],
         ["01", {"0","1"}, 'q0', {'q1'}, {('q0', '1'): 'q1', ('q0', '0'): 'q1'}, False],
-        #...
+        
+        # Test case 4: Testing different DFA 
         ["010101", {"0", "1"}, 'q0', {'q2'}, {('q0', '1'): 'q1', ('q0', '0'): 'q2', ('q1', '1'): 'q1', ('q1', '0'): 'q2', ('q2', '1'): 'q2', ('q2', '0'): 'q2'}, True],
 
-        # # Test case 5: DFA with a loop and different start and accepting states
-        # ["01010", {"0", "1"}, 'q1', {'q1'}, {('q1', '1'): 'q1', ('q1', '0'): 'q2', ('q2', '1'): 'q2', ('q2', '0'): 'q1'}, True],
+        # Test case 5: DFA with a loop and different start and accepting states
+        ["010100", {"0", "1"}, 'q1', {'q1'}, {('q1', '1'): 'q1', ('q1', '0'): 'q2', ('q2', '1'): 'q2', ('q2', '0'): 'q1'}, True],
 
         # Test case 6: DFA with an invalid character
         ["0231", {"0", "1"}, 'q0', {'q1'}, {('q0', '1'): 'q1', ('q0', '0'): 'q1'}, False],
